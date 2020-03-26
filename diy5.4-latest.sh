c4ffd4840b78e3eef9f28e58a12da366d2bfeab6
zxlhhyccc <zxlhhy@gmail.com>
2020-03-26 18:32:53 +0800

    5.4

--- diy5.4-latest.sh
+++ diy5.4-latest.sh
@@ -7,11 +7,12 @@
 #=================================================
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-# 添加默认编译包
-rm -f ./include/target.mk
-wget -P ./include/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/include/target.mk
-rm -f ./include/netfilter.mk
-wget -P ./include/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/include/netfilter.mk
+# 删除openwrt-latest自带的插件
+# rm -rf ./package/ctcgew
+# rm -rf ./package/lean
+# rm -rf ./package/lienol
+# rm -rf ./package/ntlf9t
+# rm -rf ./package/zxlhhyccc
 # 替换文件应用新IP为192.168.50.1及默认中文及设置登录密码为admin
 rm -f ./package/base-files/files/bin/config_generate
 wget -P ./package/base-files/files/bin/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/base-files/files/bin/config_generate
@@ -21,43 +22,15 @@ wget -P ./package/base-files/files/etc/ https://raw.githubusercontent.com/zxlhhy
 # 开启wifi
 rm -f ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 wget -P ./package/kernel/mac80211/files/lib/wifi/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/files/lib/wifi/mac80211.sh
-# 替换network中为19.07的odhcpd源码包解决dns解析导致无法获取dns不能上网问题
-rm -rf ./package/network/services/odhcpd
-svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/19.07/network/services/odhcpd package/network/services/odhcpd
-# 修改fullconenat加速模块Makefile适配5.4内核
-rm -rf ./package/openwrt-package/lean/openwrt-fullconenat
-svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/package/lean/openwrt-fullconenat package/openwrt-package/lean/openwrt-fullconenat
-# 添加bcm53xx默认内核为5.4（在4.14情况下.config确定）
-rm -f ./target/linux/bcm53xx/Makefile
-wget -P ./target/linux/bcm53xx/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.14-5.4-bcm53xx/Makefile
-# 添加x86默认编译包及内核为5.4（在4.14情况下.config确定)
-rm -f ./target/linux/x86/Makefile
-wget -P ./target/linux/x86/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.14-5.4-x86/Makefile
-# 添加bcm53xx默认内核为5.4（在4.19情况下.config确定）
-rm -f ./target/linux/bcm53xx/Makefile
-wget -P ./target/linux/bcm53xx/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.19-5.4-bcm53xx/Makefile
-# 添加x86默认编译包及内核为5.4（在4.19情况下.config确定)
-rm -f ./target/linux/x86/Makefile
-wget -P ./target/linux/x86/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.19-5.4-x86/Makefile
-# 添加upx压缩源码
-svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/tools/ucl tools/ucl
-svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/tools/upx tools/upx
-rm -f ./tools/Makefile
-wget -P ./tools/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/tools/Makefile
 # 去除feeds中的material主题多余固件名
 rm -f ./feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/header.htm
 wget -P ./feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/header.htm
 # 将tty所在服务目录改到系统目录
 rm -f ./feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
 wget -P ./feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
-# 添加feeds里的依赖包
-svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/lang/python/Flask-RESTful feeds/packages/lang/python/Flask-RESTful
-# 修改feeds中ksmbd的makefile适配5.4内核
-rm -f ./feeds/packages/kernel/ksmbd/Makefile
-wget -P ./feeds/packages/kernel/ksmbd/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/packages/kernel/ksmbd/Makefile
 # 升级feeds中的exfat-nofuse源码
-#rm -rf ./feeds/packages/kernel/exfat-nofuse
-#svn co  https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/kernel/exfat-nofuse feeds/packages/kernel/exfat-nofuse
+ rm -rf ./feeds/packages/kernel/exfat-nofuse
+ svn co  https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/kernel/exfat-nofuse feeds/packages/kernel/exfat-nofuse
 # 删除feeds里的与自有包冲突插件
 rm -rf ./feeds/packages/net/frp
 rm -rf ./feeds/packages/net/kcptun
@@ -65,4 +38,47 @@ rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/utils/syncthing
 rm -rf ./feeds/luci/applications/luci-app-frpc
 rm -rf ./feeds/luci/applications/luci-app-frps
-rm -rf ./feeds/luci/applications/luci-app-ksmbd
+rm -rf ./packages/lean/openwrt-kcptun
+rm -rf ./packages/zxlhhyccc/luci-app-syncthing
+rm -rf ./packages/zxlhhyccc/openwrt-syncthing
+rm -rf ./packages/zxlhhyccc/verysync
+rm -rf ./packages/ntlf9t/luci-app-smartdns
+rm -rf ./packages/ntlf9t/smartdns
+rm -rf ./packages/ctcgfw/open-app-filter
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/lean/frp package/lean/frp
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/lean/kcptun package/lean/kcptun
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/lean/proxychains-ng package/lean/proxychains-ng
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/luci-app-syncthing package/zxlhhyccc/luci-app-syncthing
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/syncthing package/zxlhhyccc/syncthing
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-frpc package/lean/luci-app-frpc
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/luci-app-frps package/tcgfw/luci-app-frps
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/luci-app-k3screenctrl-master package/tcgfw/luci-app-k3screenctrl-master
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/Szt-k3sreenctrl-master package/zxlhhyccc/Szt-k3sreenctrl-master
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/luci-app-k3usb-master package/zxlhhyccc/luci-app-k3usb-master
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/verysync package/zxlhhyccc/verysync
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/openclash package/zxlhhyccc/openclash
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/AdGuardHome-53xx package/zxlhhyccc/AdGuardHome-53xx
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/AdGuardHome-x64 package/zxlhhyccc/AdGuardHome-x64
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/lean/luci-app-dnspod package/lean/luci-app-dnspod
+
+svn co  https://github.com/pymumu/luci/trunk/applications/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ntlf9t/smartdns package/ntlf9t/smartdns
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/zxlhhyccc/OpenAppFilter/open-app-filter package/ctcgfw/open-app-filter
+
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-mod package/ctcgfw/bbr-mod
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-bbrplus package/ctcgfw/bbr-bbrplus
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-nanqinlang package/ctcgfw/bbr-nanqinlang
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-tsunami package/ctcgfw/bbr-tsunami
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-mod-4.19 package/ctcgfw/bbr-mod-4.19
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-nanqinlang-4.19 package/ctcgfw/bbr-nanqinlang-4.19
+svn co  https://github.com/zxlhhyccc/bf-package/trunk/ctcgfw/bbr-tsunami-4.19 package/ctcgfw/bbr-tsunami-4.19
+
+
