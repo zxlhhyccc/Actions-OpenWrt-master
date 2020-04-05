75fd43d43cd625ec44322b966a542118aea1b5d5
zxlhhyccc <zxlhhy@gmail.com>
2020-04-05 14:10:08 +0800

    fix sqm

--- diy5.4.sh
+++ diy5.4.sh
@@ -87,7 +87,10 @@ rm -rf ./feeds/packages/net/transmission-web-control
 svn co  https://github.com/project-openwrt/packages-latest/trunk/net/transmission-web-control feeds/packages/net/transmission-web-control
 rm -rf ./feeds/luci/applications/luci-app-transmission
 svn co  https://github.com/project-openwrt/luci-latest/trunk/applications/luci-app-transmission feeds/luci/applications/luci-app-transmission
-# 修改freeswitch-stable和kamailio-5.x依赖为python3
+# 修改sqm-scripts汉化help
+rm -rf ./feeds/packages/net/sqm-scripts
+svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/net/sqm-scripts feeds/packages/net/sqm-scripts
+# 修改kamailio-5.x依赖为python3
 rm -f ./feeds/telephony/net/kamailio-5.x/Makefile
 wget -P ./feeds/telephony/net/kamailio-5.x/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/telephony/net/kamailio-5.x/Makefile
 # 删除feeds里的与自有包冲突插件
