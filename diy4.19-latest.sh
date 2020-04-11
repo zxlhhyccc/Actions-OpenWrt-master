cb6eb058488c5acdb7b80fdcba6bfe7492a17ce5
zxlhhyccc <zxlhhy@gmail.com>
2020-04-11 20:08:05 +0800

    add libcpus

--- diy4.19-latest.sh
+++ diy4.19-latest.sh
@@ -24,6 +24,7 @@ wget -P ./feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/ htt
 # 升级feeds中的exfat-nofuse源码
 rm -rf ./feeds/packages/kernel/exfat-nofuse
 svn co  https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/kernel/exfat-nofuse feeds/packages/kernel/exfat-nofuse
+svn co  https://github.com/openwrt/packages/trunk/libs/libcups feeds/packages/libs/libcups
 # 修改mwan3检测IP
 rm -f ./feeds/packages/net/mwan3/files/etc/config/mwan3
 wget -P ./feeds/packages/net/mwan3/files/etc/config/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/packages/net/mwan3/files/etc/config/mwan3
