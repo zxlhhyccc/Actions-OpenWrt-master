95b173e8eb7bc78bbeac118498f727265a1d4134
zxlhhyccc <zxlhhy@gmail.com>
2020-10-19 19:54:22 +0800

    diy5.4.sh:update

--- diy5.4.sh
+++ diy5.4.sh
@@ -11,11 +11,6 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i 's/processors 1/processors $(shell nproc)/g' include/image.mk
 # openssl：通过以下方式，使ARMv8设备适配ChaCha20-Poly1305而不是AES-GCM
 sed -i 's/default y if !x86_64 && !aarch64/default y if !x86_64/g' package/libs/openssl/Config.in
-# luci-compat: 在不足的ACL上启用旧式CBI表单
-# sed -i 's/disabled/Enable/g' feeds/luci/modules/luci-compat/luasrc/view/cbi/footer.htm
-# 替换map.htm适配启用旧式CBI
-# rm -f ./feeds/luci/modules/luci-compat/luasrc/view/cbi/map.htm
-# wget -P ./feeds/luci/modules/luci-compat/luasrc/view/cbi/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/luci/modules/luci-compat/luasrc/view/cbi/map.htm
 # 添加默认编译包
 rm -f ./include/target.mk
 wget -P ./include/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/include/target.mk
@@ -28,8 +23,6 @@ rm -f ./package/kernel/linux/modules/netfilter.mk
 wget -P ./package/kernel/linux/modules/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/linux/modules/netfilter.mk
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 sed -i 's/7440/7200/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
-# rm -f ./package/kernel/linux/files/sysctl-nf-conntrack.conf
-# wget -P ./package/kernel/linux/files/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/linux/files/sysctl-nf-conntrack.conf
 # 修改network中防火墙等源码包
 rm -rf ./package/network/config/firewall
 svn co https://github.com/project-openwrt/openwrt/branches/master/package/network/config/firewall package/network/config/firewall
@@ -53,8 +46,6 @@ wget -P ./feeds/luci/modules/luci-mod-network/ https://raw.githubusercontent.com
 pushd feeds/luci/modules/luci-mod-network
 patch -p1 < 001-wifi-add-MU-MIMO-option.patch
 popd
-# rm -rf ./feeds/luci/applications/luci-app-firewall
-# svn co https://github.com/project-openwrt/luci/trunk/applications/luci-app-firewall feeds/luci/applications/luci-app-firewall
 # 更新htop、libyaml-cpp
 rm -rf ./feeds/packages/admin/htop
 svn co https://github.com/project-openwrt/packages/trunk/admin/htop feeds/packages/admin/htop
@@ -62,15 +53,15 @@ rm -rf ./feeds/packages/libs/libyaml-cpp
 svn co https://github.com/project-openwrt/packages/trunk/libs/libyaml-cpp feeds/packages/libs/libyaml-cpp
 # 添加5.4内核ACC、shortcut-fe补丁
 rm -f ./target/linux/generic/hack-5.4/250-netfilter_depends.patch
-wget -P ./target/linux/generic/pending-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/250-netfilter_depends.patch
+wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/250-netfilter_depends.patch
 rm -f ./target/linux/generic/hack-5.4/650-netfilter-add-xt_OFFLOAD-target.patch
-wget -P ./target/linux/generic/pending-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/650-netfilter-add-xt_OFFLOAD-target.patch
+wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/650-netfilter-add-xt_OFFLOAD-target.patch
 rm -f ./target/linux/generic/hack-5.4/661-use_fq_codel_by_default.patch
-wget -P ./target/linux/generic/pending-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/661-use_fq_codel_by_default.patch
+wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/661-use_fq_codel_by_default.patch
 rm -f ./target/linux/generic/hack-5.4/662-remove_pfifo_fast.patch
 wget -P ./target/linux/generic/pending-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/662-remove_pfifo_fast.patch
 rm -f ./target/linux/generic/hack-5.4/721-phy_packets.patch
-wget -P ./target/linux/generic/pending-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/721-phy_packets.patch
+wget -P ./target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/721-phy_packets.patch
 wget -P target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
 wget -P target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
 wget -P target/linux/generic/hack-5.4/ https://raw.githubusercontent.com/project-openwrt/openwrt/master/target/linux/generic/hack-5.4/998-add-ndo-do-ioctl.patch
@@ -91,21 +82,6 @@ wget -P package/kernel/mwlwifi/patches/ https://raw.githubusercontent.com/zxlhhy
 wget -P feeds/luci/modules/luci-base/htdocs/luci-static/resources/icons/ https://github.com/zxlhhyccc/acc-imq-bbr/raw/master/master/feeds/luci/modules/luci-base/htdocs/luci-static/resources/icons/wifi_big.png
 # wireless-regdb：自定义更改txpower和dfs的补丁
 wget -P package/firmware/wireless-regdb/patches/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/firmware/wireless-regdb/patches/600-custom-change-txpower-and-dfs.patch
-# 添加bcm53xx默认内核为5.4（在4.14情况下.config确定）
-# rm -f ./target/linux/bcm53xx/Makefile
-# wget -P ./target/linux/bcm53xx/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.14-5.4-bcm53xx/Makefile
-# 添加x86默认编译包及内核为5.4（在4.14情况下.config确定)
-# rm -f ./target/linux/x86/Makefile
-# wget -P ./target/linux/x86/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.14-5.4-x86/Makefile
-# 添加bcm53xx默认内核为5.4（在4.19情况下.config确定）
-# rm -f ./target/linux/bcm53xx/Makefile
-# wget -P ./target/linux/bcm53xx/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.19-5.4-bcm53xx/Makefile
-# 打开bcm53xx的NAND解决5.4内核启动问题
-# rm -f ./target/linux/bcm53xx/config-5.4
-# wget -P ./target/linux/bcm53xx/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.19-5.4-bcm53xx/config-5.4
-# 添加x86默认编译包及内核为5.4（在4.19情况下.config确定)
-# rm -f ./target/linux/x86/Makefile
-# wget -P ./target/linux/x86/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/4.19-5.4-x86/Makefile
 # 添加upx压缩源码
 svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/tools/ucl tools/ucl
 svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/tools/upx tools/upx
@@ -113,8 +89,6 @@ rm -f ./tools/Makefile
 wget -P ./tools/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/tools/Makefile
 # 去除feeds中的material主题多余openwrt名
 sed -i 's#boardinfo.hostname or "?"#""#g' feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/header.htm
-# rm -f ./feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/header.htm
-# wget -P ./feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/luci/themes/luci-theme-material/luasrc/view/themes/material/header.htm
 # 将tty、ksmd所在服务目录改到系统、网络存储目录
 sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
 sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
@@ -123,15 +97,6 @@ svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/lang
 svn co https://github.com/project-openwrt/packages/trunk/libs/opencv feeds/packages/libs/opencv
 svn co https://github.com/openwrt/packages/branches/openwrt-19.07/libs/fcgi feeds/packages/libs/fcgi
 svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/libs/libdouble-conversion feeds/packages/libs/libdouble-conversion
-# 替换oaf适配5.4内核
-# rm -rf ./package/openwrt-package/zxlhhyccc/OpenAppFilter/oaf
-# svn co https://github.com/project-openwrt/openwrt-latest/trunk/package/ctcgfw/oaf package/openwrt-package/zxlhhyccc/OpenAppFilter/oaf
-# 升级feeds中的exfat-nofuse源码
-rm -rf ./feeds/packages/kernel/exfat-nofuse
-svn co  https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/kernel/exfat-nofuse feeds/packages/kernel/exfat-nofuse
-# 修改mwan3检测IP
-# rm -f ./feeds/packages/net/mwan3/files/etc/config/mwan3
-# wget -P ./feeds/packages/net/mwan3/files/etc/config/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/packages/net/mwan3/files/etc/config/mwan3
 # 添加dnamasq的IPV6展示
 wget -P ./package/network/services/dnsmasq/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/network/services/dnsmasq/patches/0001-dnsmasq-add-filter-aaaa-option.patch
 pushd package/network/services/dnsmasq
@@ -153,10 +118,6 @@ wget -P ./target/linux/ramips/ https://raw.githubusercontent.com/zxlhhyccc/acc-i
 pushd target/linux/ramips
 patch -p1 < 000-k2p-network.patch
 popd
-# wget -P ./target/linux/ramips/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/ramips/patches/001-mt7621-enable-hw-crypto.patch
-# pushd target/linux/ramips
-# patch -p1 < 001-mt7621-enable-hw-crypto.patch
-# popd
 wget -P ./target/linux/ramips/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/target/linux/ramips/patches/0003-mt7621.dtsi-add-missing-pinctrl-to-ethernet-node.patch
 pushd target/linux/ramips
 patch -p1 < 0003-mt7621.dtsi-add-missing-pinctrl-to-ethernet-node.patch
@@ -172,27 +133,18 @@ wget -P ./feeds/luci/applications/luci-app-transmission/ https://raw.githubuserc
 pushd feeds/luci/applications/luci-app-transmission
 patch -p1 < 001-luci-app-transmission-with-dbengine.patch
 popd
-# rm -rf ./feeds/packages/net/transmission
-# svn co  https://github.com/project-openwrt/packages/trunk/net/transmission feeds/packages/net/transmission
-# rm -rf ./feeds/packages/net/transmission-web-control
-# svn co  https://github.com/project-openwrt/packages/trunk/net/transmission-web-control feeds/packages/net/transmission-web-control
-# rm -rf ./feeds/luci/applications/luci-app-transmission
-# svn co  https://github.com/project-openwrt/luci/trunk/applications/luci-app-transmission feeds/luci/applications/luci-app-transmission
-# wget -P ./feeds/packages/net/transmission/patches/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/packages/net/transmission/patches/007-transmission-init.patch
 # 修改sqm-scripts汉化help
-rm -rf ./feeds/packages/net/sqm-scripts
-svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/net/sqm-scripts feeds/packages/net/sqm-scripts
+svn co https://github.com/zxlhhyccc/acc-imq-bbr/trunk/master/feeds/packages/net/sqm-scripts/patches feeds/packages/net/sqm-scripts/patches
+rm -rf ./feeds/packages/net/sqm-scripts/patches/.svn
 # 修复新版luci的cpu等寄存器显示
 wget -P ./feeds/luci/modules/luci-mod-status/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/luci/modules/luci-mod-status/patches/001-luci-mod-status-fix-register-functions.patch
 pushd feeds/luci/modules/luci-mod-status
 patch -p1 < 001-luci-mod-status-fix-register-functions.patch
 popd
-# rm -f ./feeds/luci/modules/luci-mod-network/root/usr/share/rpcd/acl.d/luci-mod-network.json
-# wget -P ./feeds/luci/modules/luci-mod-network/root/usr/share/rpcd/acl.d/ https://raw.githubusercontent.com/project-openwrt/luci/master/modules/luci-mod-network/root/usr/share/rpcd/acl.d/luci-mod-network.json
-# rm -f ./feeds/luci/modules/luci-mod-status/root/usr/share/rpcd/acl.d/luci-mod-status.json
-# wget -P ./feeds/luci/modules/luci-mod-status/root/usr/share/rpcd/acl.d/ https://raw.githubusercontent.com/project-openwrt/luci/master/modules/luci-mod-status/root/usr/share/rpcd/acl.d/luci-mod-status.json
-# rm -f ./feeds/luci/modules/luci-mod-system/root/usr/share/rpcd/acl.d/luci-mod-system.json
-# wget -P ./feeds/luci/modules/luci-mod-system/root/usr/share/rpcd/acl.d/ https://raw.githubusercontent.com/project-openwrt/luci/master/modules/luci-mod-system/root/usr/share/rpcd/acl.d/luci-mod-system.json
+wget -P ./feeds/luci/modules/luci-mod-status/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/feeds/luci/modules/luci-mod-status/patches/002-luci-mod-status-drop-lluci.ver-display.patch
+pushd feeds/luci/modules/luci-mod-status
+patch -p1 < 002-luci-mod-status-drop-lluci.ver-display.patch
+popd
 # 添加netdata显示中文日期补丁及升级到1.22.1
 # sed -i 's/1.20.0/1.22.1/g' feeds/packages/admin/netdata/Makefile
 # sed -i 's/c739e0fa8d6d7f433c0c7c8016b763e8f70519d67f0b5e7eca9ee5318f210d90/6efd785eab82f98892b4b4017cadfa4ce1688985915499bc75f2f888765a3446/g' feeds/packages/admin/netdata/Makefile
@@ -241,10 +193,10 @@ rm -rf ./package/openwrt-package/lean/nft-qos
 # unzip luci-app-flowoffload-master-NAT.zip
 # popd
 # 打开wifi并设置区域为US、设置频道为157
-wget -P ./package/kernel/mac80211/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/patches/000-wifi-auto.patch
-pushd package/kernel/mac80211
-patch -p1 < 000-wifi-auto.patch
-popd
+# wget -P ./package/kernel/mac80211/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/patches/001-wifi-auto.patch
+# pushd package/kernel/mac80211
+# patch -p1 < 001-wifi-auto.patch
+# popd
 # mac80211：为ath / subsys：在2g上允许vht添加补丁
 wget -P ./package/kernel/mac80211/patches/subsys/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/patches/subsys/600-mac80211-allow-vht-on-2g.patch
 wget -P ./package/kernel/mac80211/patches/ath/ https://raw.githubusercontent.com/zxlhhyccc/acc-imq-bbr/master/master/package/kernel/mac80211/patches/ath/983-ath10k-allow-vht-on-2g.patch
